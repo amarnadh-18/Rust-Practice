ba9947c43ff4df2ec52d4b5a869ad7e23ee13a36
Amarnath <nnagsadasivamurthy@gmail.com>
2025-09-15 06:05:00 +0000

    Updated

--- src/ownership/mod.rs
+++ src/ownership/mod.rs
@@ -1,3 +1,8 @@
 pub fn run(){
-    
+    //let x = &String::from("Hello World");
+    let x = String::from("Hello World");
+    //let y = x.clone();
+    //let y = x.as_str();
+    let y = &x;
+    println!("{}, {}", x, y);
 }
